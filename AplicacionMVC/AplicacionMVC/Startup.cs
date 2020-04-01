80b32a4dcf490559209c9b1e22d3b5784cc2173a
ralejo <ronald152515@gmail.com>
2020-04-01 13:31:42 -0500

    2DA SUBIDA

---
+++ AplicacionMVC/AplicacionMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AplicacionMVC.Startup))]
+namespace AplicacionMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
