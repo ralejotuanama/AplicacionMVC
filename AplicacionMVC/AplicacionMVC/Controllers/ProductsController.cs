5efeea927dd62d5c82ee8e06767d9ff4d45bbf68
ralejo <ronald152515@gmail.com>
2020-04-01 14:17:35 -0500

    modificacion en controller ProductsController para la logica

--- AplicacionMVC/AplicacionMVC/Controllers/ProductsController.cs
+++ AplicacionMVC/AplicacionMVC/Controllers/ProductsController.cs
@@ -46,7 +46,7 @@ namespace AplicacionMVC.Controllers
         // más información vea http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "ProductID,Descripcion,LastBuy,Quantity")] Product product)
+        public ActionResult Create([Bind(Include = "ProductID,Descripcion,LastBuy,Quantity,Price")] Product product)
         {
             if (ModelState.IsValid)
             {
@@ -78,7 +78,7 @@ namespace AplicacionMVC.Controllers
         // más información vea http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "ProductID,Descripcion,LastBuy,Quantity")] Product product)
+        public ActionResult Edit([Bind(Include = "ProductID,Descripcion,LastBuy,Quantity,Price")] Product product)
         {
             if (ModelState.IsValid)
             {
