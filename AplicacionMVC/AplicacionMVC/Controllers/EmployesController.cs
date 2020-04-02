09b194086722bdc1ae39649185727505682a13df
ralejo <ronald152515@gmail.com>
2020-04-01 19:24:31 -0500

    Add Controller Employes

---
+++ AplicacionMVC/AplicacionMVC/Controllers/EmployesController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using AplicacionMVC.Models;
+
+namespace AplicacionMVC.Controllers
+{
+    public class EmployesController : Controller
+    {
+        private MVCContextType db = new MVCContextType();
+
+        // GET: Employes
+        public ActionResult Index()
+        {
+            var employes = db.Employes.Include(e => e.DocumentType);
+            return View(employes.ToList());
+        }
+
+        // GET: Employes/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Employe employe = db.Employes.Find(id);
+            if (employe == null)
+            {
+                return HttpNotFound();
+            }
+            return View(employe);
+        }
+
+        // GET: Employes/Create
+        public ActionResult Create()
+        {
+            ViewBag.DocumentTypeID = new SelectList(db.DocumentTypes, "DocumentTypeID", "Description");
+            return View();
+        }
+
+        // POST: Employes/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "EmployeId,FirstName,LastName,Salary,DateOfBirth,DocumentTypeID")] Employe employe)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Employes.Add(employe);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.DocumentTypeID = new SelectList(db.DocumentTypes, "DocumentTypeID", "Description", employe.DocumentTypeID);
+            return View(employe);
+        }
+
+        // GET: Employes/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Employe employe = db.Employes.Find(id);
+            if (employe == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.DocumentTypeID = new SelectList(db.DocumentTypes, "DocumentTypeID", "Description", employe.DocumentTypeID);
+            return View(employe);
+        }
+
+        // POST: Employes/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "EmployeId,FirstName,LastName,Salary,DateOfBirth,DocumentTypeID")] Employe employe)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(employe).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.DocumentTypeID = new SelectList(db.DocumentTypes, "DocumentTypeID", "Description", employe.DocumentTypeID);
+            return View(employe);
+        }
+
+        // GET: Employes/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Employe employe = db.Employes.Find(id);
+            if (employe == null)
+            {
+                return HttpNotFound();
+            }
+            return View(employe);
+        }
+
+        // POST: Employes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Employe employe = db.Employes.Find(id);
+            db.Employes.Remove(employe);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
